7e82187d326a2e8d80872f26ba72b86f1afcb777
jaskon139 <jaskon139@gmail.com>
2021-03-17 20:39:03 +0800

    sdfsdfsdf

--- configure.sh
+++ configure.sh
@@ -2,6 +2,8 @@
 # V2Ray new configuration
 #echo "$CONFIG_JSON" > /etc/v2ray/config.json
 
+cd /app
+
 
 #!/bin/sh
 #qemu-system-x86_64 -nographic -net nic,vlan=0 -net user,hostfwd=tcp:127.0.0.1:8888-:22 -m 512 -hda /app/ssh_and_ss/tc/tc.img < /dev/null &
@@ -22,8 +24,6 @@
 echo "hello"
 
 chmod +x ./*
-
-./rungit.sh &
 
 while true
 do 
