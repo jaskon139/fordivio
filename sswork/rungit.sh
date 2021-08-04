21a5d6d0745b7d0c494b6b40bc7baf92cef0a9aa
jaskon139 <jaskon139@gmail.com>
2021-08-05 07:06:45 +0800

    55

--- sswork/rungit.sh
+++ sswork/rungit.sh
@@ -1,43 +1,20 @@
 #!/bin/bash
-cd /app
-
-echo "rungit"
 
 while true
 do
 
-sleep 1400m
+sleep 1400
 
-export KUBECONFIG=./okteto-kube.config
+cd /app
 
-okteto stack deploy --wait
-
-#./kubectl apply -f ./k8s.yml
-
+    if [ ! -n $gitdir ]; then  
+        echo "IS NULL"  
+    else
+        rm -rf $gitname
+        echo "IS not NULL"  
+        git clone $gitdir  
+        cd $gitname
+        chmod +x configrun.sh
+        ./configrun.sh
+    fi
 done
-
-
-
-
-
-# while /bin/true
-# do
-#         currentHour=$(date +%H)
-#         if [ $currentHour -eq 19 ]
-#         then
-        
-#         dataline=$(cat README.md)
-#         current=$(date +%d)
-
-#         if [ $dataline -eq $current ]
-#         then
-#            echo $(date +%Y-%m-%d);
-#         else
-#                 date +%d > README.md
-#         fi
-#         else
-#         echo $(date +%Y-%m-%d);
-#         fi
-#         sleep 55m
-# done
-#/app/v2ray-v3.31-linux-64/v2ray -co
